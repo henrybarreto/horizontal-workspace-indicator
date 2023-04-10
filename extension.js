c71837b193e8ba74a851cf029f5db0067ec4f9a6
Henry Barreto <me@henrybarreto.dev>
2023-04-10 20:56:19 -0300

    wip

--- extension.js
+++ extension.js
@@ -1,23 +1,12 @@
-const { Clutter, St, GObject, Pango } = imports.gi;
+const Me = imports.misc.extensionUtils.getCurrentExtension();
+const ExtensionUtils = imports.misc.extensionUtils;
 const Main = imports.ui.main;
 const PanelMenu = imports.ui.panelMenu;
+const { Clutter, St, GObject, Pango, Gio } = imports.gi;
 
-const ExtensionUtils = imports.misc.extensionUtils;
-
-const symbolsMap = {
-    circles: {
-        active: "●",
-        inactive: "○"
-    },
-    lines: {
-        active: "┃",
-        inactive: "ǀ"
-    }
-}
-
-const leftButton = 1;
-const middleButton = 2;
-const rightButton = 3;
+const HOME_ICON = 'go-home';
+const WORKSPACE_ICON = 'computer';
+const ADD_ICON = 'list-add-symbolic';
 
 let WorkspaceIndicator = GObject.registerClass(
     class WorkspaceIndicator extends PanelMenu.Button {
@@ -26,107 +15,105 @@ let WorkspaceIndicator = GObject.registerClass(
 
             this._settings = ExtensionUtils.getSettings();
 
-            this._setIcons();
-            
             this._container = new St.Widget({
                 layout_manager: new Clutter.BinLayout(),
                 x_expand: true,
                 y_expand: true,
                 style_class: "widget",
             });
+
             this.add_actor(this._container);
-            
-            this._statusLabel = new St.Label({
-                style_class: "panel-button-text",
-                text: this.getWidgetText(),
-                x_expand: true,
-                y_expand: true,
-                y_align: Clutter.ActorAlign.CENTER,
-            });
-            this._statusLabel.clutter_text.line_wrap = true;
-            this._statusLabel.clutter_text.line_wrap_mode = Pango.WrapMode.CHAR;
-            this._statusLabel.clutter_text.ellipsize = Pango.EllipsizeMode.NONE;
-            this._container.add_actor(this._statusLabel);
+
+			this._box = new St.BoxLayout({
+				vertical: false,
+				style_class: 'workspace-indicator-box',
+			});
+			this._container.add_actor(this._box);
 
             let workspaceManager = global.workspace_manager;
             this._workspaceManagerSignals = [
                 workspaceManager.connect_after('notify::n-workspaces',
-                    this._updateView.bind(this)),
+                    this.update.bind(this)),
                 workspaceManager.connect_after('workspace-switched',
-                    this._updateView.bind(this)),
+                    this.update.bind(this)),
             ];
 
-            this.connect('button-press-event', this._onButtonPress);
+			this._icons = [];
+
+			for (let i = 0; i < workspaceManager.n_workspaces; i++) {
+				this._icons.push(new St.Icon({
+					icon_name: HOME_ICON,
+					style_class: 'system-status-icon',
+				}));
+
+				this._box.add_actor(this._icons[i]);
+
+				this.setHomeIcon(workspaceManager, i);
+			}
+
+			this.setPlusIcon(workspaceManager);
         }
 
-        _setIcons() {
-            this._icons = {};
+		// setHomeIcon set the HOME_ICON to current workspace, otherwise set WORKSPACE_ICON.
+		//
+		// @param workspaceManager Meta.WorkspaceManager.
+		// @param workspaceIndex number.
+		setHomeIcon(workspaceManager, workspaceIndex) {
+			if (workspaceManager.get_active_workspace().index() === workspaceIndex) {
+				this._icons[workspaceIndex].set_icon_name(HOME_ICON);
+			} else {
+				this._icons[workspaceIndex].set_icon_name(WORKSPACE_ICON);
+			}
+		}
 
-            switch (this._settings.get_string("icons-style")) {
-                case "lines":
-                    this._icons.active = symbolsMap.lines.active;
-                    this._icons.inactive = symbolsMap.lines.inactive;
-                    break;
-                default:
-                    this._icons.active = symbolsMap.circles.active;
-                    this._icons.inactive = symbolsMap.circles.inactive;
-            }
+		// setPlusIcon set the ADD_ICON to the last workspace.
+		//
+		// @param workspaceManager Meta.WorkspaceManager.
+		setPlusIcon(workspaceManager) {
+			this._icons[this._icons.length-1].set_icon_name(ADD_ICON);
+		}
 
-            switch (this._settings.get_string("widget-orientation")) {
-                case "vertical":
-                    this._icons.separator = "\n";
-                    break;
-                default:
-                    this._icons.separator = "";
-            }
+        update() {
+			log("update");
+            let workspaceManager = global.workspace_manager;
+			log(this._icons.length);
+			log(workspaceManager.n_workspaces);
+			if (this._icons.length < workspaceManager.n_workspaces) {
+				log("enter on condition");
+				this._icons.push(new St.Icon({
+					icon_name: WORKSPACE_ICON,
+					style_class: 'system-status-icon',
+				}));
+				this._box.add_actor(this._icons[this._icons.length-1]);
+			}
+
+			if (this._icons.length > workspaceManager.n_workspaces) {
+				this._box.remove_actor(this._icons[this._icons.length-1]);
+				this._icons.pop();
+			}
+
+			for (let i = 0; i < this._icons.length; i++) {
+				this.setHomeIcon(workspaceManager, i);
+			}
+
+			this.setPlusIcon(workspaceManager);
         }
 
         destroy() {
             for (let i = 0; i < this._workspaceManagerSignals.length; i++)
-                global.workspace_manager.disconnect(this._workspaceManagerSignals[i])
+                workspaceManager.disconnect(this._workspaceManagerSignals[i])
             super.destroy();
-        }
-
-        _updateView() {
-            this._statusLabel.text = this.getWidgetText();
-        }
-
-        _onButtonPress(_, event) {
-            let workspaceManager = global.workspace_manager;
-            let activeWorkspaceIndex = workspaceManager.get_active_workspace_index();
-            let button = event.get_button();
-
-            if (button == leftButton) {
-                if (activeWorkspaceIndex == 0) {
-                    return
-                }
-                Main.wm.actionMoveWorkspace(workspaceManager.get_workspace_by_index(activeWorkspaceIndex-1));
-            } else if (button == rightButton) {
-                if (activeWorkspaceIndex == workspaceManager.get_n_workspaces()-1) {
-                    return
-                }
-                Main.wm.actionMoveWorkspace(workspaceManager.get_workspace_by_index(activeWorkspaceIndex+1));
-            } else if (button == middleButton) {
-                if (Main.overview.visible) {
-                    Main.overview.hide();
-                } else {
-                    Main.overview.show();
-                }
-            }
-        }
-
-        getWidgetText() {
-            let items = [];
-            let numberWorkspaces = global.workspace_manager.get_n_workspaces();
-            let currentWorkspaceIndex = global.workspace_manager.get_active_workspace_index();
-
-            for (let i = 0; i < numberWorkspaces; i++) {
-                items.push(i == currentWorkspaceIndex ? this._icons.active : this._icons.inactive);
-            }
-            return items.join(this._icons.separator)
         }
     }
 );
+
+function getWidgetIndex(position) {
+    if (position === "right") {
+        return 0
+    } 
+
+    return 1
+}
 
 class Extension {
     constructor(uuid) {
@@ -143,14 +130,6 @@ class Extension {
         this._indicator.destroy();
         this._indicator = null;
     }
-}
-
-function getWidgetIndex(position) {
-    if (position === "right") {
-        return 0
-    } 
-
-    return 1
 }
 
 function init(meta) {
